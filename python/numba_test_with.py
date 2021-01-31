133b6492fb7ae5dc0b56c81e85a1147073a18959
BrunoASNascimento <bruno-asn@outlook.com.br>
2021-01-31 12:05:46 -0300

    Upload numba

---
+++ python/numba_test_with.py
@@ -0,0 +1,20 @@
+import time
+from numba import njit
+
+
+@njit(fastmath=True, cache=True)
+def sum_pair_numbers(num_Max):
+    sum_pair = 0
+    control = 0
+    while control < num_Max:
+        if control % 2 == 0:
+            sum_pair += control
+        control += 1
+    return sum_pair
+
+
+if __name__ == '__main__':
+    start = time.time()
+    print(f'Sum: {sum_pair_numbers(1000000000)}')
+    end = time.time()
+    print(f'{end-start} s')
