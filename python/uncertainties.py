0f2528d82271784b9d15ac7f6dc8399ac02dd5c4
BrunoASNascimento <bruno-asn@outlook.com.br>
2021-02-15 18:53:44 -0300

    add uncertainties

---
+++ python/uncertainties.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+data_list = [
+    {
+        'value': 0.1,
+        'error': 0.01,
+        'square': 4
+    },
+    {
+        'value': 0.01,
+        'error': 0.002,
+        'square': 2
+    }
+]
+
+value_calc = 1
+error_calc = 0
+
+for data in data_list:
+    value_calc *= data['value']**data['square']
+    error_calc += (data['square']*(data['error']/data['value']))**2
+
+error_calc = value_calc*np.sqrt(error_calc)
+
+print(f'{value_calc} ± {error_calc}')
