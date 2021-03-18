c1eefcf6aa1bc644c237aae43e12d01c5bacd1c0
BrunoASNascimento <bruno-asn@outlook.com.br>
2021-03-18 01:23:54 -0300

    Visualize Loop Progress in Python

---
+++ python/print_loops.py
@@ -0,0 +1,6 @@
+from tqdm import tqdm
+import time
+
+for idx in tqdm(range(0, 10)):
+    time.sleep(0.1)
+# print(idx, value)
--- python/uncertainties.py
+++ python/uncertainties.py
@@ -17,9 +17,11 @@ value_calc = 1
 error_calc = 0
 
 for data in data_list:
+    # print(data) #! Print data to log
     value_calc *= data['value']**data['square']
     error_calc += (data['square']*(data['error']/data['value']))**2
 
 error_calc = value_calc*np.sqrt(error_calc)
+
 
 print(f'{value_calc} ± {error_calc}')
