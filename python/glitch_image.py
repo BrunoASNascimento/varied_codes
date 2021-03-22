509d3860a89ac5d0d5f3cf14e0b9c83a58907378
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-03-22 19:21:48 -0300

    glitch_image added

---
+++ python/glitch_image.py
@@ -0,0 +1,25 @@
+from glitch_this import ImageGlitcher
+from PIL import Image
+from numba import njit
+
+
+PATH = 'data/IMG-20181224-WA0006.jpg'
+
+glitcher = ImageGlitcher()
+
+img = Image.open(PATH)
+glitch_img = glitcher.glitch_image(
+    img, 8, color_offset=True, gif=True, scan_lines=False, frames=60)
+
+# glitch_img.save('data/443580_test.jpg')
+
+
+DURATION = 10     # Set this to however many centiseconds each frame should be visible for
+LOOP = 0            # Set this to how many times the gif should loop
+# LOOP = 0 means infinite loop
+glitch_img[0].save('data/glitched_test.gif',
+                   format='GIF',
+                   append_images=glitch_img[1:],
+                   save_all=True,
+                   duration=DURATION,
+                   loop=LOOP)
