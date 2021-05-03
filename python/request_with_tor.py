94a70a199ad5d7e68cd2318a2644edfb6cf12775
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-05-02 21:24:21 -0300

    Added request with tor

---
+++ python/request_with_tor.py
@@ -0,0 +1,21 @@
+# Open directory TOR -> cd C:\Users\bruno\Tor Browser\Browser\TorBrowser\Tor
+# Deactivate service -> tor --service remove
+# Activate service -> tor --service install -options ControlPort 9151
+# List all conections -> netstat -an
+
+import random
+import requests
+
+
+def check_ip():
+    session = requests.session()
+    creds = str(random.randint(10000, 0x7fffffff)) + ":" + "foobar"
+    session.proxies = {
+        'http': 'socks5h://{}@localhost:9050'.format(creds),
+        'https': 'socks5h://{}@localhost:9050'.format(creds)
+    }
+    r = session.get('http://httpbin.org/ip')
+    print(r.text)
+
+
+check_ip()
