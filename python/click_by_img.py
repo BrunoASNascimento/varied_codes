06afa32f035f547b8bc0eca8c8d303b5fa388841
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-05-06 18:12:42 -0300

    Update streamlit

--- python/click_by_img.py
+++ python/click_by_img.py
@@ -3,7 +3,7 @@ import pyautogui
 
 def click_fig(img_name):
     print(f'[INFO] Read image: {img_name}')
-    button_location = pyautogui.locateOnScreen(img_name, confidence=0.7)
+    button_location = pyautogui.locateOnScreen(img_name, confidence=0.9)
     print(button_location)
     center_point = pyautogui.center(button_location)
     print(center_point)
