26ad86dbc85757e5060dbc3230847bb95703dc9d
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-05-06 18:27:03 -0300

    Update function in streamlit

--- python/streamlit_ping.py
+++ python/streamlit_ping.py
@@ -2,17 +2,18 @@ import streamlit as st
 import os
 import platform
 
-current_os = platform.system().lower()
-print(current_os)
-if current_os == "windows":
-    parameter = "-n"
-else:
-    parameter = "-c"
 
-ip = "127.0.0.1"
-exit_code = os.system(f"ping {ip}")
-print(exit_code)
-if exit_code == 0:
-    st.text(f'{ip} -> OK')
-else:
-    st.text(f'{ip} -> FAIL')
+def check_ping(hostname):
+
+    response = os.system("ping -c 1 " + hostname)
+    # and then check the response...
+    if response == 0:
+        pingstatus = "Network Active"
+    else:
+        pingstatus = "Network Error"
+
+    return pingstatus
+
+
+hostname = st.text_input('Input your hostname here:')
+st.text(f'{hostname} -> {check_ping(hostname)}')
