2cb00edcbb75a42ff4a716ea6a81499f4ca811a5
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-07-04 23:09:50 -0300

    Selenium test

---
+++ python/selenium_without_display.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+
+# Links
+
+# https://medium.com/@pyzzled/running-headless-chrome-with-selenium-in-python-3f42d1f5ff1d
+# https://stackoverflow.com/questions/50642308/webdriverexception-unknown-error-devtoolsactiveport-file-doesnt-exist-while-t
+# https://stackoverflow.com/questions/53073411/selenium-webdriverexceptionchrome-failed-to-start-crashed-as-google-chrome-is
+# https://stynxh.github.io/2020-06-21-install-google-chrome-on-ubuntu-linux-from-command-line-eng/
+# https://stynxh.github.io/2019-10-09-how-to-run-chromedriver-in-selenium-in-docker-with-python3/
+# https://hub.docker.com/r/selenium/node-chrome/dockerfile/
+# https://stackoverflow.com/questions/43665276/how-to-run-google-chrome-headless-in-docker
+
+
+# instantiate a chrome options object so you can set the size and headless preference
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+# download the chrome driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put it in the
+# current directory
+chrome_driver = '/usr/local/bin/chromedriver'
+# go to Google and click the I'm Feeling Lucky button
+driver = webdriver.Chrome(chrome_options=chrome_options,
+                          executable_path=chrome_driver)
+driver.get("https://www.google.com")
+lucky_button = driver.find_element_by_xpath('//*[@id="gb"]/div/div[2]/a')
+lucky_button.click()
+
+# capture the screen
+driver.get_screenshot_as_file("capture.png")
+driver.close()
--- python/xuxu.py
+++ python/xuxu.py
@@ -1,11 +1,18 @@
-import numpy as np
-x = 20.0*1000
-y = 0.2*1000
-z = 24.0*1000
+# import numpy as np
+# x = 20.0*1000
+# y = 0.2*1000
+# z = 24.0*1000
 
-while (x <= z):
-    print('branch_1')
-    print(np.around(x % 1000, 2) == 0)
-    print(np.around(x % 1, 2)-0.99 == 0)
-    x += y
-print(z)
+# while (x <= z):
+#     print('branch_1')
+#     print(np.around(x % 1000, 2) == 0)
+#     print(np.around(x % 1, 2)-0.99 == 0)
+#     x += y
+# print(z)
+
+
+test = '2 5 6 9 8 7 4 2 5 8 6 6'
+list_a = test.split(' ')
+print([int(i) for i in list_a])
+print(list(map(int, list_a)))
+print(list_a[::-1])
