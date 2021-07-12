fe6199bd8dc05e8734b05036fcf080786ea5ad06
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-07-12 09:20:36 -0300

    Update files

--- python/xuxu.py
+++ python/xuxu.py
@@ -11,8 +11,22 @@
 # print(z)
 
 
-test = '2 5 6 9 8 7 4 2 5 8 6 6'
-list_a = test.split(' ')
-print([int(i) for i in list_a])
-print(list(map(int, list_a)))
-print(list_a[::-1])
+# test = '2 5 6 9 8 7 4 2 5 8 6 6'
+# list_a = test.split(' ')
+# print([int(i) for i in list_a])
+# print(list(map(int, list_a)))
+# print(list_a[::-1])
+
+
+x = [6.0, 1.4, 1.2, 7.1, 4.6, 2.4, 6.8, 3.3, 6.8, 6.3,
+     1.2, 8.2, 3.1, 9.9, 3.9, 7.3, 7.3, 2.9, 9.3, 2.3]
+y = [3.1, 3.2, 0.3, 2.7, 2.1, 0.0, 0.0, 1.4, 1.4, 3.2,
+     0.0, 1.8, 4.7, 0.6, 1.4, 3.2, 0.3, 1.8, 4.0, 3.2]
+cdm = 0.0
+n = 20
+for i in range(n):
+    cdm += (
+        (x[i]*y[n-1-i])*(i+1)/(8)
+    )
+
+print(cdm)
